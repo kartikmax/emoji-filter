c90b9366cde34c390d6c602a1c7638e6ff1e6014
kartikmax <kyadav5213@gmail.com>
2023-07-14 20:55:09 +0530

    added filter

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,25 @@ import "./app.css";
 const App = () => {
   const [emojis, setEmojis] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [filter, setFilter] = useState({
+    group: "",
+    category: "",
+  });
+  const [filteredEmojis, setFilteredEmojis] = useState([]);
+  const [availableGroups, setAvailableGroups] = useState([]);
+  const [availableCategories, setAvailableCategories] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    applyFilter();
+  }, [emojis, filter]);
+
+  useEffect(() => {
+    extractFilterOptions();
+  }, [emojis]);
 
   const fetchData = async () => {
     try {
@@ -21,47 +36,113 @@ const App = () => {
     }
   };
 
-  const convertEmoji = (unicode) => {
-    const codePoint = unicode.substring(2); // Remove the "U+" prefix
-    const parsedCodePoint = parseInt(codePoint, 16);
+  const applyFilter = () => {
+    let filteredData = emojis;
 
-    if (
-      isNaN(parsedCodePoint) ||
-      parsedCodePoint < 0x0 ||
-      parsedCodePoint > 0x10ffff
-    ) {
-      return ""; // Return an empty string for invalid code points
+    if (filter.group) {
+      filteredData = filteredData.filter(
+        (emoji) => emoji.group === filter.group
+      );
     }
 
-    const convertedEmoji = String.fromCodePoint(parsedCodePoint);
-    return convertedEmoji;
+    if (filter.category) {
+      filteredData = filteredData.filter(
+        (emoji) => emoji.category === filter.category
+      );
+    }
+
+    setFilteredEmojis(filteredData);
   };
 
-  const memoizedEmojis = useMemo(() => emojis, [emojis]);
+  const extractFilterOptions = () => {
+    const groups = [...new Set(emojis.map((emoji) => emoji.group))];
+    const categories = [...new Set(emojis.map((emoji) => emoji.category))];
+    setAvailableGroups(groups);
+    setAvailableCategories(categories);
+  };
+
+  const handleFilterChange = (event) => {
+    const { name, value } = event.target;
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      [name]: value,
+    }));
+  };
+
+  const handleFilterSubmit = (event) => {
+    event.preventDefault();
+    applyFilter();
+  };
+
+  const memoizedEmojis = useMemo(() => filteredEmojis, [filteredEmojis]);
 
   return (
-    <div className="emoji-container">
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        memoizedEmojis.map((emoji) => (
-          <div className="emoji-card" key={emoji.name + emoji.category}>
-            <span
-              className="emoji"
-              role="img"
-              aria-label={emoji.name}
-              dangerouslySetInnerHTML={{ __html: emoji.htmlCode[0] }}
-            />
-            <div className="emoji-details">
-              <p>Name: {emoji.name}</p>
-              <p>Category: {emoji.category}</p>
-              <p>Group: {emoji.group}</p>
-              <p>Emoji: {convertEmoji(emoji.htmlCode[0])}</p>
+    <>
+      <h1>Emoji Application</h1>
+      <div className="navbar">
+        <form onSubmit={handleFilterSubmit}>
+          <label htmlFor="groupFilter">Group:</label>
+          <select
+            id="groupFilter"
+            name="group"
+            value={filter.group}
+            onChange={handleFilterChange}
+          >
+            <option value="">All</option>
+            {availableGroups.map((group) => (
+              <option value={group} key={group}>
+                {group}
+              </option>
+            ))}
+          </select>
+
+          <label htmlFor="categoryFilter">Category:</label>
+          <select
+            id="categoryFilter"
+            name="category"
+            value={filter.category}
+            onChange={handleFilterChange}
+          >
+            <option value="">All</option>
+            {availableCategories.map((category) => (
+              <option value={category} key={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+
+          <button type="submit">Filter</button>
+        </form>
+      </div>
+      <div>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : memoizedEmojis.length > 0 ? (
+          <>
+            <p>{`${memoizedEmojis.length} emojis filtered`}</p>
+            <div className="emoji-container">
+              {memoizedEmojis.map((emoji) => (
+                <div className="emoji-card" key={emoji.name + emoji.category}>
+                  <span
+                    className="emoji"
+                    role="img"
+                    aria-label={emoji.name}
+                    dangerouslySetInnerHTML={{ __html: emoji.htmlCode[0] }}
+                  />
+                  <div className="emoji-details">
+                    <p>Name: {emoji.name}</p>
+                    <p>Category: {emoji.category}</p>
+                    <p>Group: {emoji.group}</p>
+                  </div>
+                </div>
+              ))}
             </div>
-          </div>
-        ))
-      )}
-    </div>
+          </>
+        ) : (
+          <p>No emojis found.</p>
+        )}
+      </div>
+    </>
   );
 };
 
