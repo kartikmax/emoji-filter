1eaf80c5c3f90966f1b2bff8eabd0f069e26ffe4
kartikmax <kyadav5213@gmail.com>
2023-07-14 20:16:30 +0530

    added the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import "./app.css";
 
 const App = () => {
   const [emojis, setEmojis] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchData();
@@ -14,6 +15,7 @@ const App = () => {
       const response = await axios.get("https://emojihub.yurace.pro/api/all");
       const data = response.data;
       setEmojis(data);
+      setIsLoading(false);
     } catch (error) {
       console.error("Error fetching emojis:", error);
     }
@@ -35,21 +37,30 @@ const App = () => {
     return convertedEmoji;
   };
 
+  const memoizedEmojis = useMemo(() => emojis, [emojis]);
+
   return (
     <div className="emoji-container">
-      {emojis.map((emoji) => (
-        <div className="emoji-card" key={emoji.name + emoji.category}>
-          <span className="emoji" role="img" aria-label={emoji.name}>
-            {convertEmoji(emoji.htmlCode[0])}
-          </span>
-          <div className="emoji-details">
-            <p>Name: {emoji.name}</p>
-            <p>Category: {emoji.category}</p>
-            <p>Group: {emoji.group}</p>
-            <p>Emoji: {convertEmoji(emoji.htmlCode[0])}</p>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        memoizedEmojis.map((emoji) => (
+          <div className="emoji-card" key={emoji.name + emoji.category}>
+            <span
+              className="emoji"
+              role="img"
+              aria-label={emoji.name}
+              dangerouslySetInnerHTML={{ __html: emoji.htmlCode[0] }}
+            />
+            <div className="emoji-details">
+              <p>Name: {emoji.name}</p>
+              <p>Category: {emoji.category}</p>
+              <p>Group: {emoji.group}</p>
+              <p>Emoji: {convertEmoji(emoji.htmlCode[0])}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
